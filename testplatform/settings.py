ff911d9b6857b384c335c016dab8ecf17299e3ae
liuxiang <905364660@qq.com>
2020-07-15 17:08:16 +0800

    完成自己写的注册登录功能

--- testplatform/settings.py
+++ testplatform/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '74raofmi10-qf*$*tf!3@u%a(t9&zp(yi97)d30cn5*(z8*!xc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -44,7 +45,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
--- users/views.py
+++ users/views.py
@@ -1,30 +1,32 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from users.models import Users
 from django import views
 
 
 class UserLoginView(views.View):
 
-    def get(self, request):
-        return JsonResponse({"data": "你好GET请求"},status=200)
-
-
     def post(self, request):
-        if request.method == "POST":
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            data = Users.objects.filter(username=username, password=password)
-            if data:
-                return JsonResponse({"data":"success"}, status=200)
-        return JsonResponse({"data": "error"}, status=400)
+        json_data = request.body.decode('utf-8')
+        python_data = eval(json_data)
+        username = python_data.get('username')
+        password = python_data.get('password')
+        data = Users.objects.filter(username=username, password=password).first()
+        if data:
+            dict = {"id": data.id, "username": data.username}
+            return JsonResponse(dict, status=200)
+        return JsonResponse({"data": "无效用户名或密码"}, status=400)
 
 
 class UserRegisterView(views.View):
+
     def post(self, request):
-        data = request
-        new_user = Users.objects.create(**data)
-        dict = {
-            "id": new_user.id,
-            "username": new_user.username
-        }
-        return JsonResponse(dict, status=201)
+        json_data = request.body.decode('utf-8')
+        python_data = eval(json_data)
+        if python_data.get("username") and python_data.get("password") and python_data.get("email"):
+            try:
+                new_user = Users.objects.create(**python_data)
+                dict = {"id": new_user.id, "username": new_user.username}
+                return JsonResponse(dict, status=201)
+            except Exception as e:
+                return JsonResponse({"data_error": str(e)}, status=400)
+        return JsonResponse({"data": "所有字段为必填项"}, status=400)
