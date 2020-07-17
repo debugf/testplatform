d54e199b4a6fbb1b98f33a6057bdf6c8934f4f82
liuxiang <905364660@qq.com>
2020-07-17 09:44:37 +0800

    删掉自己写登录功能

--- users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from users import views
-
-urlpatterns = [
-    path('login/', views.UserLoginView.as_view()),
-    path('register/', views.UserRegisterView.as_view())
-]
